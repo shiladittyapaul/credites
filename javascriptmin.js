9824db5ced2b31a07f8e16971917f30a711f2587
Shiladittya Paul <shiladittyapaul@gmail.com>
2018-01-26 02:07:56 +0530

    new

---
+++ javascriptmin.js
@@ -0,0 +1,13 @@
+<script type='text/javascript'>
+$(document).ready(function()
+{
+var aa=$("#mycredit").val();
+if (aa == null) {
+    window.location.href = "http://spaul.org";
+};
+ $("#mycredit").attr("href","
+http://spaul.org");
+
+});
+
+</script>
